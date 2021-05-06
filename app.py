a2cd0d664294e631818ae20e977dbe3021ff844f
JonnyVärn <jonnywaern@hotmail.com>
2021-05-06 16:35:55 +0200

    something

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
-from flask import Response, request
-from flask_api import status
+#import os
+#import subprocess
+from flask import Flask, render_template
+#from flask import Response, request
+#from flask_api import status
 
 #create Flask instance
 app = Flask(__name__)
 
 @app.route('/')
-def hello_World():
-    hostname = socket.gethostname()
-    return f'Hello World container{hostname}'
+def index():
+    return render_template("Hello")
+def ip():
+    return reqests.host()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', port=80, debug=True)
