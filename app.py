1453bc660b1088935f79e691e4eb4029eb29f7ec
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 15:33:13 +0200

    something changed

--- app.py
+++ app.py
@@ -18,8 +18,14 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def containerRunning():
     hostname = socket.gethostname()
-    return f'it working {hostname} \n'
+    return f'its working {hostname} \n'
     
+@app.route('/headers')
+    def headers():
+        for header in request.headers:
+            result = result + f'{header[0]}:\t{header[1]}\n'
+            return Response(result, mimetype='text/plain')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
