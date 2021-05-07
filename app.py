591883ad525ca32c651f7fb64a190a02214b0f4b
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 19:20:24 +0200

    something changed

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-import os
-#import subprocess
+# import os
+# import subprocess
+# from flask import render_template
 import socket
-from flask import Flask, render_template
+from flask import Flask, 
 from flask import Response, request
-from flask_api import status
+# from flask_api import status
 
 
-#create Flask instance
+# create Flask instance
 app = Flask(__name__)
-
-
-
-@app.route('/')
-
-
 
 @app.route('/', methods=['POST'])
 def containerRunning():
@@ -26,7 +21,6 @@ def headers():
     for header in request.headers:
         result = result + f'{header[0]}:\t{header[1]}\n'
         return Response(result, mimetype='text/plain')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
