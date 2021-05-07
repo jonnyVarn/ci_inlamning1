78b05bfc5871a97a24a4f281125540889b7f09fc
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 19:51:35 +0200

    something changed

--- app.py
+++ app.py
@@ -17,15 +17,18 @@ hostname = socket.gethostname()
 @app.route('/', methods=['POST', 'GET'])
 def containerRunning():
     return f'its working {hostname}'
+
     
-    
-# @app.route('/headers')
-# def headers():
-#    result = ""
-#    for header in request.headers:
-#        result = result + f'{header[0]}:\t{header[1]}\n'
-#        return Response(result, mimetype='text/plain')
+@app.route('/headers')
+def headers():
+    result = ""
+    for header in request.headers:
+    result = result + f'{header[0]}:\t{header[1]}\n'
+    return Response(result, mimetype='text/plain')
 
 
-# if (__name__) == "__main__":
-#    app.run(host='0.0.0.0', port=80, debug=True)
+if (__name__) == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+
+    # i hate pep8..
