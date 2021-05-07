ce398006bdcc8d1899dc3059fc3ea2862a0b260a
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 22:15:57 +0200

    whitspace shit

--- test_app.py
+++ test_app.py
@@ -2,7 +2,6 @@ import app
 import requests
 
 
-
 class AppTester():
-    t1=request.get("http://127.0.0.1")
+    t1 = request.get("http://127.0.0.1")
     assert r.status_code == 200, “API did not start correctly”
