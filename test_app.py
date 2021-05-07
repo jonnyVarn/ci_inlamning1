468a44dc33081c3737811fcee518c633e79a7f2b
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 22:24:07 +0200

    strange test2

--- test_app.py
+++ test_app.py
@@ -4,4 +4,7 @@ import requests
 
 class AppTester():
     t1 = requests.get("http://127.0.0.1")
-    assert r.status_code == 200, 'No Work'
+    assert t1.status_code == 200, 'No Work'
+
+    t2 = app.testar
+    assert t2 == "ok"
