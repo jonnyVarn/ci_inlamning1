84137dc2dcf1e1dcaf09ab35e4f6da4816a0d4d9
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 23:11:43 +0200

    copy pasting

--- test_app.py
+++ test_app.py
@@ -1,5 +1,12 @@
-import app
+from app import create_app
 
-class ptest():
-    t1 = app.container
-    assert t1.__eq__ == "its working"
+def test_home_page():
+    flask_app = create_app('flask_test.cfg')
+
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
+        assert b"Welcome to the Flask User Management Example!" in response.data
+        assert b"Need an account?" in response.data
+        assert b"Existing user?" in response.data
+        
