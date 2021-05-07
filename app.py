d8675ac421fc5cc2811b2248ba9bdd0ff122d26a
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 19:46:35 +0200

    something changed

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def containerRunning():
-    hostname = socket.gethostname()
-    return f'its working {hostname}'
-
+#    hostname = socket.gethostname()
+#    return f'its working {hostname}'
+    return "pep8 suger fett"
     
-@app.route('/headers')
-def headers():
-    result = ""
-    for header in request.headers:
-        result = result + f'{header[0]}:\t{header[1]}\n'
-        return Response(result, mimetype='text/plain')
+# @app.route('/headers')
+# def headers():
+#    result = ""
+#    for header in request.headers:
+#        result = result + f'{header[0]}:\t{header[1]}\n'
+#        return Response(result, mimetype='text/plain')
 
-        
+
 # if (__name__) == "__main__":
 #    app.run(host='0.0.0.0', port=80, debug=True)
