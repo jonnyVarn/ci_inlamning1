8b8fd32d920c31c577e03e85f972869c278444fa
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 15:20:48 +0200

    something changed

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM python:3-alpine
-RUN apk add --update python3 py3-pip py3-flask; pip3 install --no-cache-dir flask flask-api
+RUN apk add --update python3 py3-pip py3-flask; pip3 install --no-cache-dir flask flask-api gunicorn
 ADD ./app.py ./app.py 
+ADD ./input.html ./input.html
 #ENV PYTHONPATH /usr/lib/python3.9/site-packages 
 CMD python3 app.py
+EXPOSE 5000
 EXPOSE 80
 
 
--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 #import os
 #import subprocess
 from flask import Flask, render_template
-#from flask import Response, request
-#from flask_api import status
+from flask import Response, request
+from flask_api import status
+
 
 #create Flask instance
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def index():
-    return render_template("Hello")
-def ip():
-    return reqests.host()
+
+
+
+@app.route('/', methods=['POST'])
+def containerRunning():
+    hostname= socket.gethostname()
+    return f'it working {hostname} \n'
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
