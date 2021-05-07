452bdde9e4b8ea7708e3b05af46a13bd97840427
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 19:22:17 +0200

    something changed

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def headers():
         result = result + f'{header[0]}:\t{header[1]}\n'
         return Response(result, mimetype='text/plain')
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
