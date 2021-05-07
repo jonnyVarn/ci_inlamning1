371cd5836f56a33e2c985672d032216cd72511e8
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 22:37:49 +0200

    testing without requests

--- test_app.py
+++ test_app.py
@@ -3,8 +3,10 @@ import app
 
 
 class AppTester():
+     t2 = app.testar
+    assert t2 == "ok", 'working'
+
 #    t1 = requests.get("http://127.0.0.1:80")
 #    assert t1.status_code == 200, 'No Work'
 
-    t2 = app.testar
-    assert t2 == "ok", 'working'
+   
