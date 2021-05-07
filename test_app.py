fb36c3ba676c23a11fa6b67c41e3f546f7c4b3ef
JonnyVärn <jonnywaern@hotmail.com>
2021-05-07 22:17:44 +0200

    instead ''

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ import requests
 
 class AppTester():
     t1 = request.get("http://127.0.0.1")
-    assert r.status_code == 200, “API did not start correctly”
+    assert r.status_code == 200, 'No Work'
