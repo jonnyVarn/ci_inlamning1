0cad7615f97286b68b59cb4577d57e1c5f0a48aa
JonnyVärn <jonnywaern@hotmail.com>
2021-09-03 15:38:23 +0200

    format test_app.py

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 from app import create_app
 
+
 def test_home_page():
     flask_app = create_app('flask_test.cfg')
-
 
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
