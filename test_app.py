c5880b8c5ff961ad12e793920f6fde6293143e55
JonnyVärn <jonnywaern@hotmail.com>
2021-09-03 15:48:49 +0200

    trying # noqa E501 line too long (80 > 79 characters)

--- test_app.py
+++ test_app.py
@@ -5,8 +5,8 @@ def test_home_page():
     flask_app = create_app('flask_test.cfg')
 
     with flask_app.test_client() as test_client:
-        response = test_client.get('/')
-        assert response.status_code == 200
-        assert b"Welcome to the Flask User Management Example!" in response.data
-        assert b"Need an account?" in response.data
-        assert b"Existing user?" in response.data
+        response = test_client.get('/') # noqa
+        assert response.status_code == 200 # noqa
+        assert b"Welcome to the Flask User Management Example!" in response.data # noqa
+        assert b"Need an account?" in response.data # noqa
+        assert b"Existing user?" in response.data # noqa
